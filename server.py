9742450a906fac1bc4210f3d74a18674295c99fe
Krishna <krishnayad997@gmail.com>
2020-04-01 16:17:57 +0800

    performed migration

--- server.py
+++ server.py
@@ -1,17 +1,21 @@
+import peeweedbevolve # new; must be imported before models
 from flask import Flask, render_template, request
-from models import db # new line
-
+from models import db
 
 app = Flask(__name__)
 
-@app.before_request # new line
+@app.before_request
 def before_request():
    db.connect()
 
-@app.after_request # new line
+@app.after_request
 def after_request(response):
    db.close()
    return response
+
+@app.cli.command() # new
+def migrate(): # new 
+   db.evolve(ignore_tables={'base_model'}) # new
 
 @app.route("/")
 def index():
