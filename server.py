ccdeb537f023c38f4045ba11b7f7d0b5c8b1f996
Krishna <krishnayad997@gmail.com>
2020-04-02 13:26:57 +0800

    Can add warehouse to specofic stores

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import peeweedbevolve # new; must be imported before models
-from flask import Flask, render_template, request
-from models import db
+from flask import Flask, render_template, request,redirect
+from models import db, Store, Warehouse
+
 
 app = Flask(__name__)
 
@@ -20,6 +21,27 @@ def migrate(): # new
 @app.route("/")
 def index():
    return render_template('index.html')
+
+@app.route("/store")
+def store():
+   return render_template('store.html')
+
+@app.route("/store", methods=["POST"])
+def store_add():
+   Store.create(name=request.form.get("store_name"))
+   return redirect("/store")
+
+@app.route("/warehouse")
+def warehouse(): 
+   store = Store.select()
+   return render_template('warehouse.html',store=store)
+
+@app.route("/warehouse", methods=["POST"])
+def warehouse_add():
+   print(request.form["store_id"]) 
+   store = Store.get_by_id(request.form["store_id"])
+   Warehouse.create(location=request.form.get("warehouse_location"),store=store)
+   return redirect("/warehouse")
 
 if __name__ == '__main__':
    app.run()
